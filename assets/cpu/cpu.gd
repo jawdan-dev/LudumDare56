a4b4bfcdcce5296169379cd7265908f81e292ff0
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2024-10-07 15:41:39 +0100

    Removed debug stuffs + set kart & environment collision layers.

--- assets/cpu/cpu.gd
+++ assets/cpu/cpu.gd
@@ -5,8 +5,6 @@ func getAccelerationInput(): return 1;
 var lastCheckpoint : int = 0;
 var targetPoint : Vector3 = Vector3.ZERO;
 
-@export var debugBlock : Node3D;
-
 func getTurningInput(): 
 	if (!checkpointParent): return 0;
 	
@@ -17,7 +15,6 @@ func getTurningInput():
 		var checkpoint : Node3D = checkpointParent.get_child(lastCheckpoint);		
 		targetPoint = checkpoint.global_position
 		targetPoint += Vector3(cos(checkpoint.global_rotation.y), 0, sin(checkpoint.global_rotation.y)) * randf_range(-1, 1) * 0.5;
-		debugBlock.global_position = targetPoint;
 	
 	var to : Vector3 = global_position - targetPoint;
 	var targetAngle : float = (0.25 * TAU) - atan2(to.z, to.x);
